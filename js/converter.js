dfeb3da9c9c8f0649ab047b0bfa99501de17446d
Raul Alejandro Garcia Ortiz <raul03ortz@gmail.com>
2025-02-28 20:05:14 -0600

    Actualizacion de aplicacion web 28 de febrero

---
+++ js/converter.js
@@ -0,0 +1,54 @@
+const inputField = document.getElementById('input-temp');
+const fromUnitField = document.getElementById('input-unit');
+const toUnitField = document.getElementById('output-unit');
+const outputFField = document.getElementById('output-temp');
+const form = document.getElementById('converter');
+
+function converTemp(value,fromUnit, toUnit){
+    /**
+     * CONVERTIR DE CELCIUS A FARENHEIT O KELVIN
+     */
+    if(fromUnit=== 'c'){
+        if(toUnit=== 'f'){
+            return value * 9 /5+32;
+        }else if(toUnit === 'k'){
+            return value + 273.15;
+        }
+        return value;
+    }
+
+    /**
+     * CONVERTIR DE FARENHEIT A CELSIUS O KELVIN
+     */
+    if(fromUnit=== 'f'){
+        if(toUnit === 'c'){
+            return (value-32)*5/9;
+        }else if(toUnit === 'k'){
+            return (value + 459.67) *5 /9;
+        }
+        return value;
+    }
+
+    /**
+     * CONVERTIR DE KELVIN A FARENHEIT O CELSIUS
+     */
+
+    if(fromUnit=== 'k'){
+        if(toUnit === 'c'){
+            return value-273.15;
+        }else if(toUnit === 'f'){
+            return value *9 /5 - 459.67;
+        }
+        return value;
+    }
+    throw new Error('Unidad de Temperatura Invalida');
+}
+
+form.addEventListener('input',()=> {
+    const inputTemp = parseFloat(inputField.value);
+    const fromUnit = fromUnitField.value;
+    const toUnit = toUnitField.value;
+    const outputTemp = converTemp(inputTemp, fromUnit, toUnit);
+
+    outputField.value = (Math.round(outputTemp * 100)/1000) + ' '+ toUnit.toUpperCase();
+});
